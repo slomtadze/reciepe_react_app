1261955b98e5da3106e4993af7229805fc8ca385
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-06 13:57:10 +0000

    Update 4 files

---
+++ src/components/HomePage/Header.jsx
@@ -0,0 +1,24 @@
+const Header = () => {
+  return (
+    <div className="relative h-screen w-screen bg-herro-img bg-cover bg-center">
+      <div className="h-full w-full bg-black/30 z-5">
+        <div className="flex absolute top-[30%] left-12 text-white text-2xl z-5">
+          <div>
+            <h1 className="text-4xl font-bold leading-relaxed uppercase font-mono mr-24">
+              Looking for something new?
+              <br />{" "}
+              <span className="text-gray-200 italic">
+                We have the perfect recipes
+              </span>
+            </h1>
+            <p className="text-white animate-pulse cursor-pointer hover:text-gray-200">
+              Explore More ...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
+import Header from "../components/HomePage/Header";
+
 const Home = () => {
-  return (
-    <div className="relative h-screen w-screen bg-herro-img bg-cover bg-center">
-      <div className="absolute top-[30%] left-12 text-white text-2xl z-5">
-        test
-      </div>
-    </div>
-  );
+  return <Header />;
 };
 
 export default Home;
