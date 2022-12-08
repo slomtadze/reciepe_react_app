3c36381ae4a82509783edbc404a6a7e2cd8a2123
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-08 07:24:29 +0000

    Update Section.jsx

--- src/components/HomePage/Section.jsx
+++ src/components/HomePage/Section.jsx
@@ -3,8 +3,15 @@ import img from "../../assets/hamburger.jpg";
 
 const Section = () => {
   return (
-    <div className="bg-white w-screen flex px-40 py-24 rounded">
-      <SectionCard img={img} title="Lunch" name="lunch" />
+    <div className="w-screen flex flex-col items-center py-12 rounded">
+      <h1 className="text-4xl mb-12 font-mono font-bold">
+        Some Header Content Here
+      </h1>
+      <div className="w-[70%] flex justify-between ">
+        <SectionCard img={img} title="Lunch" name="lunch" />
+        <SectionCard img={img} title="Lunch" name="lunch" />
+        <SectionCard img={img} title="Lunch" name="lunch" />
+      </div>
     </div>
   );
 };
