d630ff4fb2c6c53e27092ee14a51bd536330dc60
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-08 12:36:49 +0000

    Update Section.jsx

--- src/components/HomePage/Section.jsx
+++ src/components/HomePage/Section.jsx
@@ -1,24 +1,25 @@
+import React from "react";
 import SectionCard from "./SectionCard";
-import img from "../../assets/hamburger.jpg";
+import useHttp from "../../hooks/use-Http";
 
-const Section = () => {
-  
-  
-  
+const Section = React.memo(({ urlPath, params }) => {
+  const { data: recipes, isLoading, error } = useHttp(urlPath, params);
+  console.log(recipes.results, isLoading, error);
+
   return (
     <div className="w-screen flex flex-col items-center py-12 rounded bg-gray-100 overflow-x-auto">
       <h1 className="text-4xl mb-12 font-mono font-bold">
         Some Header Content Here
       </h1>
       <div className="w-[90%] flex justify-around">
-        <SectionCard img={img} title="Lunch" name="lunch" />
-        <SectionCard img={img} title="Lunch" name="lunch" />
-        <SectionCard img={img} title="Lunch" name="lunch" />
-        <SectionCard img={img} title="Lunch" name="lunch" />
-        <SectionCard img={img} title="Lunch" name="lunch" />
+        {isLoading
+          ? console.log(isLoading)
+          : recipes.results.map((recipe) => (
+              <SectionCard title={recipe.name} name="lunch" />
+            ))}
       </div>
     </div>
   );
-};
+});
 
 export default Section;
