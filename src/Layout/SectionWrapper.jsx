334256f0524d13c0aa8d7b487a986e5154572819
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-12 07:46:54 +0000

    Update SectionWrapper.jsx and SectionLg.jsx

--- src/Layout/SectionWrapper.jsx
+++ src/Layout/SectionWrapper.jsx
@@ -3,8 +3,8 @@ import useHttp from "../hooks/use-Http";
 const SectionWrapper = ({ children, title }) => {
   return (
     <div className="w-full px-16 py-4">
-      <h1 className="text-center text-4xl font-mono font-bold mb-16">
-        {title}
+      <h1 className="text-center text-4xl font-bold font-mono mb-16 hover:cursor-pointer hover:text-gray-500 duration-300 ">
+        {title?.toUpperCase()}
       </h1>
       {children}
     </div>
