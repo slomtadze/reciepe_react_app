0cc69a27ffba4f055a781a41391b5e872464dc4d
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-14 13:46:07 +0000

    Update SectionListCard.jsx

--- src/components/HomePage/SectionListCard.jsx
+++ src/components/HomePage/SectionListCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SectionListCard = () => {
-  return <div className="h-[150px]">SectionListCard</div>;
+  return (
+    <div className="h-[150px] flex">
+      <div className="bg-christmas-img w-[150px] h-auto bg-cover bg-center mr-8"></div>
+      <div>
+        <h1 className="text-xl font-bold">Some header here</h1>
+        <p className="italic font-mono">some description</p>
+      </div>
+    </div>
+  );
 };
 
 export default SectionListCard;
