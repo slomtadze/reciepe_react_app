2949af1ffd851b946c4f4558e1ff71dcab0c6182
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-14 13:52:26 +0000

    Update SectionListCard.jsx

--- src/components/HomePage/SectionListCard.jsx
+++ src/components/HomePage/SectionListCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionListCard = () => {
   return (
     <div className="h-[150px] flex">
-      <div className="bg-christmas-img w-[150px] h-auto bg-cover bg-center mr-8"></div>
+      <div className="bg-christmas-img w-56 bg-cover bg-center mr-8"></div>
       <div>
         <h1 className="text-xl font-bold">Some header here</h1>
         <p className="italic font-mono">some description</p>
