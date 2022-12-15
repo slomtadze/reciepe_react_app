4eb7604e366da691334123d3dfea478a65f0ce14
Saba Lomtadze <slomtadze90@gmail.com>
2022-12-15 10:06:45 +0000

    Update 2 files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="w-full mt-4 flex justify-center">
+      <div className="w-1/4 border-t-[1px] border-gray-500 py-4 text-center">
+        <h3 className="text-lg">© 2022 All Rights Reserved.</h3>
+        <p className="italic">Created by Saba Lomtadze</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
